a72bbcc79b52c53906e078acebd1c17d106b1ea6
Sönke Noldt <soenke.noldt@partner.bmw.de>
2025-01-14 13:55:43 +0100

    make little bear large

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO ANIMALS (ID, NAME, DESCRIPTION) VALUES (1, 'Bear', 'little brown bear seen climbing tree near cabin');
+INSERT INTO ANIMALS (ID, NAME, DESCRIPTION) VALUES (1, 'Bear', 'Large brown bear seen climbing tree near cabin');
 INSERT INTO ANIMALS (ID, NAME, DESCRIPTION) VALUES (2, 'Bass', 'Large mouth bass see off the dock');
 INSERT INTO ANIMALS (ID, NAME, DESCRIPTION) VALUES (3, 'Eagle', 'Bald eagle flew over us while on our boat');
 INSERT INTO ANIMALS (ID, NAME, DESCRIPTION) VALUES (4, 'Skunk', 'Late night saw a skunk on the road after driving home from dinner');
